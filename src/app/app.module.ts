40ad6a4dabdde51da7a3417685ff3daf5fd02c7e
YunaH <YunaH503@gmail.com>
2021-07-22 16:08:20 -0400

    working address table using dummy data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { AddressComponent } from './components/address/address.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddressComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AddressComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatTableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface AddressElement {
+  locationId: number;
+  streetNumber: number;
+  streetName: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
+
+const ELEMENT_DATA: AddressElement[] = [
+  {
+    locationId: 1,
+    streetNumber: 10,
+    streetName: 'Main Street',
+    city: 'Boston',
+    state: 'MA',
+    zipcode: '02111',
+  },
+  {
+    locationId: 2,
+    streetNumber: 40,
+    streetName: 'Longwood Cir',
+    city: 'Dedham',
+    state: 'MA',
+    zipcode: '02026',
+  },
+  {
+    locationId: 3,
+    streetNumber: 55,
+    streetName: 'Broadway Drive',
+    city: 'Peabody',
+    state: 'MA',
+    zipcode: '01960',
+  },
+  {
+    locationId: 4,
+    streetNumber: 121,
+    streetName: 'Winter Avenue',
+    city: 'Cambridge',
+    state: 'MA',
+    zipcode: '02114',
+  },
+  {
+    locationId: 5,
+    streetNumber: 99,
+    streetName: 'Eliot Street',
+    city: 'Watertown',
+    state: 'MA',
+    zipcode: '02472',
+  },
+];
 
 @Component({
   selector: 'app-address',
   templateUrl: './address.component.html',
-  styleUrls: ['./address.component.css']
+  styleUrls: ['./address.component.css'],
 })
-export class AddressComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AddressComponent {
+  displayedColumns: string[] = [
+    'locationId',
+    'streetNumber',
+    'streetName',
+    'city',
+    'state',
+    'zipcode',
+  ];
+  dataSource = ELEMENT_DATA;
 }

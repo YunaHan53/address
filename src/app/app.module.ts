1f502a51f4c79eaa21aa54ca67661da7d30b0035
YunaH <YunaH503@gmail.com>
2021-07-22 09:30:47 -0400

    Added address component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddressComponent } from './components/address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule
